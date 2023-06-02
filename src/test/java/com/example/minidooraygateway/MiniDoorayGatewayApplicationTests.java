f93cb629c4b2c6353b5d560ccf13d95ca8804ecf
윤동현 <eatpaper1429@gmail.com>
2023-06-02 18:55:56 +0900

    Initial commit

---
+++ src/test/java/com/example/minidooraygateway/MiniDoorayGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minidooraygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDoorayGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
